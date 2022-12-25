f6eabc4de6c0c47ea174baf8bc035dd1f7074ffc
Song Zhaohui <zhaohui.song@studio.unibo.it>
2022-12-25 18:11:07 +0100

    add more classes

---
+++ backend/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'utils/bootstrap.php';
+
+//use Base template
+
+require 'template/base.php';
+?>
---
+++ backend/utils/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+define("UPLOAD_DIR", "./upload/");
+require_once("utils/functions.php");
+require_once("db/database.php");
+$dbh = new DatabaseHelper("localhost", "root", "", "socialMediaReddit", 3306);
+
+?>
