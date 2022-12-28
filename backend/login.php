54c0dc65807073df00bc8837ca5c2596be14edd4
Pietrell <diego.giuseppetti@gmail.com>
2022-12-28 19:54:38 +0100

    check if challenge response is correct

--- backend/login.php
+++ backend/login.php
@@ -1,24 +1,19 @@
 <?php
 require_once "utils/bootstrap.php";
 
-//use Base template
-$templateParams["pagetitle"] = "Tachyon - Login";
-$templateParams["pagename"] = "Login";
-$templateParams["usericon"] = UPLOAD_DIR . "icon.png";
-
-$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","js/login.js");
-//require 'template/base.php';
-
-
-function checkData()
-{
-    //check for input by user
-    if ( !isset($_POST['username']) ) {
-        exit('Please enter your username');
-    }
-    if ( !isset($_POST['password']) ) {
-        exit('Please enter your password');
-    }
+if(!isset($_GET['challenge'])){
+    exit();
 }
 
+$key = $dbh.getUserPass($_SESSION['Username']);
+$data = $_SESSION['challengeString'];
+
+$challengeResponse = openssl_encrypt($data, 'aes-256-gcm', $key, OPENSSL_RAW_DATA);
+
+if($challengeResponse == $_POST['challenge']){
+    echo("all right");
+    header('Location: ../backend/index.php', true, 301);
+    exit;
+}
+exit();
 ?>
