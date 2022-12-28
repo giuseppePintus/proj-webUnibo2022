8305b22ad5d7f4b116d70cd5ecad897ae569231e
Pietrell <diego.giuseppetti@gmail.com>
2022-12-28 18:21:36 +0100

    refactoring

--- backend/template/base.php
+++ backend/template/base.php
@@ -26,16 +26,16 @@
     
     <aside> 
     </aside>
-    <div class="footer-container">
     <footer>
-        <ul>
-            <li><a <?php isActive("index.php"); ?> href="index.php"><img src="upload/home.png" alt="home" /></a></li>
-            <li><a <?php isActive("search.php"); ?> href="search.php"><img src="upload/search.png" alt="search" /></a></li>
-            <li><a <?php isActive("contatti.php"); ?> href="profile.php"><img src="upload/user.png" alt="profile" /></a></li>
-            <li><a <?php isActive("login.php"); ?> href="login.php"><img src="upload/setting.png" alt="setting" /></a></li>
-        </ul>
+        <div class="footer-container">
+            <ul>
+                <li><a <?php isActive("index.php"); ?> href="index.php"><img src="upload/home.png" alt="home" /></a></li>
+                <li><a <?php isActive("search.php"); ?> href="search.php"><img src="upload/search.png" alt="search" /></a></li>
+                <li><a <?php isActive("contatti.php"); ?> href="profile.php"><img src="upload/user.png" alt="profile" /></a></li>
+                <li><a <?php isActive("login.php"); ?> href="login.php"><img src="upload/setting.png" alt="setting" /></a></li>
+            </ul>
+        </div>
     </footer>
-    </div>
     <?php
     if (isset($templateParams["js"])) :
         foreach ($templateParams["js"] as $script) :
