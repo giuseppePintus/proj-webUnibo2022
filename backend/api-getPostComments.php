1268821a9217573536c6cf80026afee8ed3490dc
Song Zhaohui <zhaohui.song@studio.unibo.it>
2022-12-30 00:02:55 +0100

    implement read comments per post

---
+++ backend/api-getPostComments.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("utils/bootstrap.php");
+$rawData = file_get_contents('php://input');
+$data = json_decode($rawData, true);
+
+$result = $dbh->getPostComments($data['commentById']);
+header("Content-Type: application/json");
+echo json_encode($result);
+?>
